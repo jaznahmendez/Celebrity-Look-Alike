5da718a2843431ee6ef18df1bd07dfc25a8a297d
jaznahmendez <jazmin.mendez@iteso.mx>
2024-07-02 21:34:42 -0600

    1st bucket up

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const multer = require('multer');
 const AWS = require('aws-sdk');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
+
 const app = express();
 const port = 3000;
 
-// Configure AWS SDK
+// Configure AWS SDK using environment variables
 AWS.config.update({
-    accessKeyId: 'YOUR_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-    region: 'YOUR_REGION'
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 
 const s3 = new AWS.S3();
@@ -36,7 +38,7 @@ app.get('/script.js', (req, res) => {
 app.post('/upload', upload.single('file'), (req, res) => {
     const file = req.file;
     const params = {
-        Bucket: 'YOUR_BUCKET_NAME',
+        Bucket: 'equipo-04-cloud',
         Key: file.originalname,
         Body: file.buffer,
         ContentType: file.mimetype,
