5f9eb6fb91d89fbef4342ac5f53b794de329fca2
jaznahmendez <jazmin.mendez@iteso.mx>
2024-07-04 23:37:57 -0600

    dynamo get

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const multer = require('multer');
 const AWS = require('aws-sdk');
 const path = require('path');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-// Configure AWS SDK using environment variables
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -15,17 +14,17 @@ AWS.config.update({
 });
 
 const s3 = new AWS.S3();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-// Configure multer for file upload
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Serve the index.html file
+let lastUploadedObject = null; // Variable to store the last uploaded object
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve CSS and JS files
 app.get('/styles.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'styles.css'));
 });
@@ -34,7 +33,6 @@ app.get('/script.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'script.js'));
 });
 
-// Upload file to S3
 app.post('/upload', upload.single('file'), (req, res) => {
     const file = req.file;
     const params = {
@@ -49,9 +47,38 @@ app.post('/upload', upload.single('file'), (req, res) => {
             console.error('Error uploading file:', err);
             return res.status(500).send('File upload failed');
         }
-        res.send('File uploaded successfully');
     });
 });
+
+let firstObject = true;
+
+const getLastUploadedObject = () => {
+    const params = {
+        TableName: 'ComparisonResults',
+        Limit: 1,
+        ScanIndexForward: false,
+    };
+
+    dynamoDB.scan(params, (err, data) => {
+        if (err) {
+            console.error('Error fetching last uploaded object:', err);
+        } else {
+            const newUploadedObject = data.Items[0];
+            if (firstObject) {
+                lastUploadedObject = newUploadedObject;
+                firstObject = false;
+            }
+            if (!lastUploadedObject || lastUploadedObject.id !== newUploadedObject.id) {
+                lastUploadedObject = newUploadedObject;
+                console.log('Updated last uploaded object:', lastUploadedObject);
+            }
+        }
+    });
+};
+
+setInterval(getLastUploadedObject, 5000);
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
